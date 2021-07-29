c06d527b968f9fdde8ce0ae032117d202fa34850
araujoG <araujo_gabriel@id.uff.br>
2021-07-28 21:16:09 -0300

    Co-authored-by: goncazevedo <jhonatan_goncalves@id.uff.br>

--- bucketSort.c
+++ bucketSort.c
@@ -1,47 +1,28 @@
 #include <stdio.h>
 
-#include "bubbleSort.c"
+#include "insertionSort.c"
 
-#define tam_bucket 2500
-#define num_bucket 10
-#define max 50
-
-typedef struct {
-	int topo;
-	int balde[tam_bucket];
-} bucket;
+#define tam_bucket 1000
+#define num_bucket 500
 
 // Bucket só pra se inspirar
-void bucket_sort(int v[], int tam) {
-	bucket b[num_bucket];
+void bucket_sort(int v[], int tam, int max) {
+	int bucket[max];
 	int i, j, k;
 
 	/* 1 */  // inicializa todos os "topo" com 0
-	for (i = 0; i < num_bucket; i++) b[i].topo = 0;
-
+	for (i = 0; i < num_bucket; i++) bucket[i] = 0;
+	
 	/* 2 */  // verifica em que balde o elemento deve ficar
-	for (i = 0; i < tam; i++) {
-		j = (num_bucket)-1;
-		while (j >= 0) {
-			if (v[i] >= j * 10) {
-				b[j].balde[b[j].topo] = v[i];
-				(b[j].topo)++;
-				break;
-			}
-			j--;
-		}
-	}
-
-	/* 3 */  // ordena os baldes
-	for (i = 0; i < num_bucket; i++)
-		if (b[i].topo) bubbleSort(b[i].balde, b[i].topo);
-
-	i = 0;
-	/* 4 */  // põe os elementos dos baldes de volta no vetor
-	for (j = 0; j < num_bucket; j++) {
-		for (k = 0; k < b[j].topo; k++) {
-			v[i] = b[j].balde[k];
-			i++;
+	for (i = 0; i < tam; i++) bucket[v[i]] = bucket[v[i]] + 1;
+	
+	/* 3 */
+	k = 0;
+	for (j = 0; j <= tam; j++)
+		while (bucket[j] != 0) {
+			bucket[j]--;
+			v[k] = j;
+			k++;
 		}
 	}
 }
--- main.c
+++ main.c
@@ -1,67 +1,84 @@
 // #include "insertionSort.c"
-#include "bucketSort.c"
-#include "mergeSort.c"
-#include "insertionSort.c"
-#include "tempoCpu.c"
 #include <string.h>
 
-void print_array(int v[], char* str, int tam){
-	printf("%s: [", str);
-	for (int i = 0; i < tam-1; i++)
-		printf("%d, ", v[i]);
-	printf("%d]\n",v[tam-1]);
+#include "bubbleSort.c"
+#include "insertionSort.c"
+#include "mergeSort.c"
+#include "tempoCpu.c"
+
+void print_array(int v[], int tam) {
+    printf("[");
+    for (int i = 0; i < tam - 1; i++) printf("%d, ", v[i]);
+    printf("%d]\n", v[tam - 1]);
 }
 
-void buildArray(int v[], int tam){
-	for (int i = 0; i < tam; i++)
-		v[i] = rand() % 50;
+void buildArray(int v[], int tam) {
+    srand(clock());
+    for (int i = 0; i < tam; i++) v[i] = rand() % 10;
 }
 
-int main(int argc, char const *argv[]){
-	double seg_CPU_inicial, seg_sistema_inicial, seg_CPU_final, seg_sistema_final;
-	// 24630 com 50 elementos
-	int tamanho = 24630; // 30 * 821
-	
-	// Bucket Sort
-	int *aux_bucket = (int*)malloc(tamanho * sizeof(int));
-	buildArray(aux_bucket, tamanho);
-	Tempo_CPU_Sistema(&seg_CPU_inicial, &seg_sistema_inicial);
-	bucket_sort(aux_bucket, tamanho);
-	Tempo_CPU_Sistema(&seg_CPU_final, &seg_sistema_final);
-	// print_array(aux_bucket,"BucketSort", tamanho);
-	printf ("\tTempo = %f\n", seg_CPU_final - seg_CPU_inicial);
-  
-	// Bubble Sort
-	int *aux_bubble = (int*)malloc(tamanho * sizeof(int));
-	printf("memcpy\n");
-	memcpy(aux_bubble,aux_bucket,tamanho*sizeof(int));
-	printf("free\n");
-	free(aux_bucket);
-	Tempo_CPU_Sistema(&seg_CPU_inicial, &seg_sistema_inicial);
-	bubbleSort(aux_bubble, tamanho);
-	Tempo_CPU_Sistema(&seg_CPU_final, &seg_sistema_final);
-	// print_array(aux_bubble,"BubbleSort", tamanho);
-	printf ("\tTempo = %f\n", seg_CPU_final - seg_CPU_inicial);
+int confereResultado(int v[], int tam) {
+    for (int i = 1; i < tam; i++)
+        if (v[(i - 1)] > v[i]) return 0;
+    return 1;
+}
 
-	//Merge Sort
-	int *aux_merge = (int*)malloc(tamanho * sizeof(int));
-	memcpy(aux_merge,aux_bubble,tamanho*sizeof(int));
-	free(aux_bubble);
-	Tempo_CPU_Sistema(&seg_CPU_inicial, &seg_sistema_inicial);
-	mergeSort(aux_merge, 0, tamanho-1);
-	Tempo_CPU_Sistema(&seg_CPU_final, &seg_sistema_final);
-	// print_array(aux_merge,"MergeSort", tamanho);
-	printf ("\tTempo = %f\n", seg_CPU_final - seg_CPU_inicial);
-	
-	//Insertion Sort
-	int *aux_insertion = (int*)malloc(tamanho * sizeof(int));
-	memcpy(aux_insertion,aux_merge,tamanho*sizeof(int));
-	free(aux_merge);
-	Tempo_CPU_Sistema(&seg_CPU_inicial, &seg_sistema_inicial);
-	insertionSort(aux_insertion, tamanho);
-	Tempo_CPU_Sistema(&seg_CPU_final, &seg_sistema_final);
-	// print_array(aux_insertion,"InsertionSort", tamanho);
-	printf ("\tTempo = %f\n", seg_CPU_final - seg_CPU_inicial);
-	free(aux_insertion);
-	return 0;
+int main(int argc, char const *argv[]) {
+    double seg_CPU_inicial, seg_sistema_inicial, seg_CPU_final,
+        seg_sistema_final;
+    // 24630 com 50 elementos
+    // int tamanho = 24630; // 30 * 821
+    int tamanho = 100;
+    int *aux = (int *)malloc(tamanho * sizeof(int));
+    buildArray(aux, tamanho);
+
+    // Bucket Sort
+    int *aux_bucket = (int *)malloc(tamanho * sizeof(int));
+    memcpy(aux_bucket, aux, tamanho * sizeof(int));
+    Tempo_CPU_Sistema(&seg_CPU_inicial, &seg_sistema_inicial);
+    // bucket_sort(aux_bucket, tamanho);
+    Tempo_CPU_Sistema(&seg_CPU_final, &seg_sistema_final);
+    printf("Bucket Sort\n");
+    print_array(aux_bucket, tamanho);
+    if (confereResultado(aux_bucket, tamanho)) printf("\tResultado Correto");
+    printf("\tTempo = %f\n", seg_CPU_final - seg_CPU_inicial);
+    free(aux_bucket);
+
+    // Bubble Sort
+    int *aux_bubble = (int *)malloc(tamanho * sizeof(int));
+    memcpy(aux_bubble, aux, tamanho * sizeof(int));
+    Tempo_CPU_Sistema(&seg_CPU_inicial, &seg_sistema_inicial);
+    bubbleSort(aux_bubble, tamanho);
+    Tempo_CPU_Sistema(&seg_CPU_final, &seg_sistema_final);
+    printf("Bubble Sort\n");
+    // print_array(aux_bubble, tamanho);
+    if (confereResultado(aux_bubble, tamanho)) printf("\tResultado Correto");
+    printf("\tTempo = %f\n", seg_CPU_final - seg_CPU_inicial);
+    free(aux_bubble);
+
+    // Merge Sort
+    int *aux_merge = (int *)malloc(tamanho * sizeof(int));
+    memcpy(aux_merge, aux, tamanho * sizeof(int));
+    Tempo_CPU_Sistema(&seg_CPU_inicial, &seg_sistema_inicial);
+    mergeSort(aux_merge, 0, tamanho - 1);
+    Tempo_CPU_Sistema(&seg_CPU_final, &seg_sistema_final);
+    printf("Merge Sort\n");
+    // print_array(aux_merge, tamanho);
+    if (confereResultado(aux_merge, tamanho)) printf("\tResultado Correto");
+    printf("\tTempo = %f\n", seg_CPU_final - seg_CPU_inicial);
+    free(aux_merge);
+
+    // Insertion Sort
+    int *aux_insertion = (int *)malloc(tamanho * sizeof(int));
+    memcpy(aux_insertion, aux, tamanho * sizeof(int));
+    free(aux);
+    Tempo_CPU_Sistema(&seg_CPU_inicial, &seg_sistema_inicial);
+    insertionSort(aux_insertion, tamanho);
+    Tempo_CPU_Sistema(&seg_CPU_final, &seg_sistema_final);
+    printf("Insertion Sort\n");
+    // print_array(aux_insertion, tamanho);
+    if (confereResultado(aux_insertion, tamanho)) printf("\tResultado Correto");
+    printf("\tTempo = %f\n", seg_CPU_final - seg_CPU_inicial);
+    free(aux_insertion);
+    return 0;
 }
